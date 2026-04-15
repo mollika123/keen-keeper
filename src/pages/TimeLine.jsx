df0243c897611041c186ab968bd7bb7bd9829f77
mollika123 <aktermollika49@gmail.com>
2026-04-15 18:07:06 +0200

    added filtering button

--- src/pages/TimeLine.jsx
+++ src/pages/TimeLine.jsx
@@ -1,15 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { TimelineTextContext } from '../context/timelineTextContext';
 import { TbPhoneCall } from 'react-icons/tb';
 import { IoIosText } from 'react-icons/io';
 import { HiMiniVideoCamera } from 'react-icons/hi2';
 
 const TimeLine = () => {
-  const {timelineText,setTimelineText} = useContext(TimelineTextContext);
-  console.log(timelineText,setTimelineText);
+  const { timelineText, setTimelineText } = useContext(TimelineTextContext);
+     const [search, setSearch] = useState('');
+  console.log(timelineText, setTimelineText);
+      const filteredCart = timelineText.filter((item) =>
+    item.type.toLowerCase().includes(search.toLowerCase())
+);
   return (
     <div className='container mx-auto py-8'>
       <h1 className='text-5xl font-bold'>Timeline</h1>
+
+           <input
+                type="text"
+                placeholder="Filter timeline..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="w-[347px] border rounded-lg px-4 py-2 mb-6 outline-none focus:ring-2 focus:ring-green-300"
+            />
       {timelineText.length===0 ? (<p className='font-bold text-center text-5xl py-12'>No Message Yet</p>):
        ( timelineText.map((text, index) => {
           return <div key={index} className='border border-dashed p-6 my-4'>
